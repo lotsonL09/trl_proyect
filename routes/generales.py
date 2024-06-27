2c80d6f1b7b82e82aa10e7efa7cb5fdd26dc8329
lotsonL09 <willimaca09ac@hotmail.com>
2024-06-27 18:16:24 -0500

    cambios-preguntas

--- routes/generales.py
+++ routes/generales.py
@@ -1,7 +1,161 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request
+from random import shuffle
 
 bp_general=Blueprint('generales',__name__,url_prefix='/generales')
 
+condictions={
+    'TRL1':1,
+    'TRL2':2,
+    'TRL3':2,
+    'TRL4':2,
+    'TRL5':2,
+    'TRL6':2,
+    'TRL7':3,
+    'TRL8':3,
+    'TRL9':3
+}
+
+data={
+    'campo_1':{
+        'title': 'Investigación',
+        'questions': [
+            {'pregunta':{
+                'enunciado':'Se ha establecido una base teórica inicial que justifica la necesidad de una investigación .',
+                'TRL': 'TRL1'}
+            },
+            {'pregunta':{
+                'enunciado':'Se ha realizado pruebas experimentales iniciales en condiciones controlada.',
+                'TRL': 'TRL3'
+            },
+        },
+            {'pregunta':{
+                'enunciado':'Se ha desarrollado modelos conceptuales que describen el funcionamiento práctico de la tecnología.',
+                'TRL': 'TRL2'
+            },
+            },
+            {'pregunta':{
+                'enunciado':'La tecnología ha demostrado eficacia en condiciones de laboratorio.',
+                'TRL': 'TRL3'
+            },
+            },
+            {'pregunta':{
+                'enunciado':'Se ha formulado una hipótesis clara y específica.',
+                'TRL': 'TRL2'
+            }
+            }
+        ],
+    },
+
+    'campo_2':{
+        'title': 'Desarrollo Tecnológico',
+        'questions': [
+            {'pregunta':{
+                'enunciado':'Ha modelado y simulado en el laboratorio para probar la tecnología planteada, obteniendo resultados positivos.',
+                'TRL': 'TRL4'}
+            },
+            {'pregunta':{
+                'enunciado':'Se ha simulado situaciones operativas reales controladas (no necesariamente en el campo o entorno de uso final) para validad la tecnología propuesta.',
+                'TRL': 'TRL5'}
+            },
+            {'pregunta':{
+                'enunciado':'Se ha colaborado con usuarios orientados para realizar pruebas operativas de la tecnología.',
+                'TRL': 'TRL6'}
+            },
+            {'pregunta':{
+                'enunciado':'Se tiene alguna documentación o reportes de las prueban en el laboratorio que muestren la funcionalidad de tu prototipo.',
+                'TRL': 'TRL4'}
+            },
+            {'pregunta':{
+                'enunciado':'Se ha demostrado que la tecnología presentada es eficiente y sostenible en un entorno operativo relevante (fuera de un entorno controlado, pero no es un entorno operativo final).',
+                'TRL': 'TRL6'}
+            },
+            {'pregunta':{
+                'enunciado':'Ha realizado evaluaciones iniciales de eficiencia y sostenibilidad de la tecnología.',
+                'TRL': 'TRL5'}
+            },
+        ]
+    },
+
+    'campo_3':{
+        'title': 'Innovación',
+        'questions': [
+            {'pregunta':{
+                'enunciado':'La tecnología propuesta ya ha sido probada y demostrada en condiciones operativas finales, es decir, en el entorno real de uso.',
+                'TRL': 'TRL7'}
+            },
+            {'pregunta':{
+                'enunciado':'Se tiene informes detallados donde se evidencie resultados positivos del desempeño de la tecnología en operaciones diarias.',
+                'TRL': 'TRL7'}
+            },
+            {'pregunta':{
+                'enunciado':'La tecnología ha sido completamente desarrollada e implementada.',
+                'TRL': 'TRL8'}
+            },
+            {'pregunta':{
+                'enunciado':'Se cuenta con los certificados emitidos por autoridades regulatorias relevantes.',
+                'TRL': 'TRL8'}
+            },
+            {'pregunta':{
+                'enunciado':'Tu tecnología está en proceso de implementación comercial o ya se encuentra en el mercado.',
+                'TRL': 'TRL9'}
+            },
+            {'pregunta':{
+                'enunciado':'La tecnología está completamente operativa y se utiliza en operaciones comerciales de manera regular.',
+                'TRL': 'TRL9'}
+            },
+        ]
+    },
+
+    'campo_4':{
+        'title': 'Desarrollo Comercial',
+        'questions': [
+        {'pregunta':{
+            'enunciado':'•	Se está recopilando y analizando datos de los usuarios/clientes para realizar las mejoras pertinentes y llevar a cabo campañas de marketing y ventas para aumentar la adopción y expandirse al mercado.',
+            'TRL': 'TRL9'}
+        },
+        {'pregunta':{
+            'enunciado':'•	Se ha desarrollado material de soporte técnico y formación para los usuarios y clientes potenciales.',
+            'TRL': 'TRL8'}
+        },
+        {'pregunta':{
+            'enunciado':'•	Se ha presentado la tecnología a potenciales clientes y/o empresas y ha recibido interés para futuras implementaciones comerciales.',
+            'TRL': 'TRL7'}
+        },
+    ]
+    },
+}
+
 @bp_general.route('/')
 def root():
-    return render_template('fields/generales.1.html')
+    shuffle(data['campo_1']['questions'])
+    shuffle(data['campo_2']['questions'])
+    shuffle(data['campo_3']['questions'])
+    shuffle(data['campo_4']['questions'])
+    return render_template('fields/generales.1.html',data=data)
+
+@bp_general.route('/evaluacion',methods=['POST'])
+def evaluation():
+    results=[]
+    count=0
+    print(request.form)
+    investigacion = request.form.getlist('Investigación')
+    desarrollo = request.form.getlist('Desarrollo Tecnológico')
+    implementacion = request.form.getlist('Innovación')
+    comercial = request.form.getlist('Desarrollo Comercial')
+    results.extend(investigacion)
+    results.extend(desarrollo)
+    results.extend(implementacion)
+    results.extend(comercial)
+    print(results)
+    for level in condictions.keys():
+        print('nivel',level)
+        print(results.count(level))
+        print('condicion',condictions[level])
+        if results.count(level) == condictions[level]:
+            count+=1
+        else:
+            break
+    if count == 0:
+        return 'No hay nivel de TRL'
+    else:
+        return f'Se encuentra en TRL{count}'

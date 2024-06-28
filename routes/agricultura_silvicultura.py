a2588372980f39f188d8364718f181f9a713f221
lotsonL09 <willimaca09ac@hotmail.com>
2024-06-27 19:17:51 -0500

    cambios ventana emergente -2

--- routes/agricultura_silvicultura.py
+++ routes/agricultura_silvicultura.py
@@ -1,7 +1,9 @@
 from flask import Blueprint,render_template,request
 from random import shuffle
+import re
 
 bp_agricultura_silvicultura=Blueprint('agricultura_silvicultura',__name__,url_prefix='/agricultura_silvicultura')
+fields=['campo_1','campo_2','campo_3','campo_4']
 
 condictions={
     'TRL1':1,
@@ -21,26 +23,26 @@ data={
         'questions': [
             {'pregunta':{
                 'enunciado':'Se ha establecido una base teórica inicial que justifica la necesidad de una investigación.',
-                'TRL': 'TRL1'}
+                'TRL': 'TRL1_0_0'}
             },
             {'pregunta':{
                 'enunciado':'Se ha realizado pruebas experimentales iniciales en condiciones controladas.',
-                'TRL': 'TRL3'
+                'TRL': 'TRL3_0_1'
             },
         },
             {'pregunta':{
                 'enunciado':'Se ha desarrollado modelos conceptuales que describen el funcionamiento práctico de la tecnología.',
-                'TRL': 'TRL2'
+                'TRL': 'TRL2_0_2'
             },
             },
             {'pregunta':{
                 'enunciado':'La tecnología ha demostrado eficacia en condiciones de laboratorio.',
-                'TRL': 'TRL3'
+                'TRL': 'TRL3_0_3'
             },
             },
             {'pregunta':{
                 'enunciado':'Se ha formulado una hipótesis clara y específica.',
-                'TRL': 'TRL2'
+                'TRL': 'TRL2_0_4'
             }
             }
         ],
@@ -51,27 +53,27 @@ data={
         'questions': [
             {'pregunta':{
                 'enunciado':'Ha modelado y simulado condiciones marinas en el laboratorio para probar la tecnología planteada, obteniendo resultados positivos.',
-                'TRL': 'TRL4'}
+                'TRL': 'TRL4_1_0'}
             },
             {'pregunta':{
                 'enunciado':'Se ha simulado situaciones operativas reales controladas (no necesariamente en el campo o entorno de uso final) para validar la tecnología propuesta.',
-                'TRL': 'TRL5'}
+                'TRL': 'TRL5_1_1'}
             },
             {'pregunta':{
                 'enunciado':'Se ha colaborado con pescadores/usuarios para realizar pruebas operativas de la tecnología.',
-                'TRL': 'TRL6'}
+                'TRL': 'TRL6_1_2'}
             },
             {'pregunta':{
                 'enunciado':'Se tiene alguna documentación o reportes de las pruebas en el laboratorio que muestren la funcionalidad de tu prototipo.',
-                'TRL': 'TRL4'}
+                'TRL': 'TRL4_1_3'}
             },
             {'pregunta':{
                 'enunciado':'Se ha demostrado que la tecnología presentada es eficiente y sostenible en un entorno operativo relevante (fuera de un entorno controlado, pero no es un entorno operativo final).',
-                'TRL': 'TRL6'}
+                'TRL': 'TRL6_1_4'}
             },
             {'pregunta':{
                 'enunciado':'Ha realizado evaluaciones iniciales de eficiencia y sostenibilidad de la tecnología.',
-                'TRL': 'TRL5'}
+                'TRL': 'TRL5_1_5'}
             },
         ]
     },
@@ -81,27 +83,27 @@ data={
         'questions': [
             {'pregunta':{
                 'enunciado':'La tecnología propuesta ya ha sido probada y demostrada en condiciones operativas finales, es decir, en el entorno real de uso.',
-                'TRL': 'TRL7'}
+                'TRL': 'TRL7_2_0'}
             },
             {'pregunta':{
                 'enunciado':'Se tiene informes detallados donde se evidencie resultados positivos del desempeño de la tecnología en operaciones diarias .',
-                'TRL': 'TRL7'}
+                'TRL': 'TRL7_2_1'}
             },
             {'pregunta':{
                 'enunciado':'La tecnología ha sido completamente desarrollada e implementada .',
-                'TRL': 'TRL8'}
+                'TRL': 'TRL8_2_2'}
             },
             {'pregunta':{
                 'enunciado':'Se cuenta con los certificados emitidos por autoridades regulatorias relevantes para el sector agrícola .',
-                'TRL': 'TRL8'}
+                'TRL': 'TRL8_2_3'}
             },
             {'pregunta':{
                 'enunciado':'Tu tecnología está en proceso de implementación comercial o ya se encuentra en el mercado agrícola.',
-                'TRL': 'TRL9'}
+                'TRL': 'TRL9_2_4'}
             },
             {'pregunta':{
                 'enunciado':'La tecnología está completamente operativa y se utiliza en operaciones comerciales de agrícolas de manera regular.',
-                'TRL': 'TRL9'}
+                'TRL': 'TRL9_2_5'}
             },
         ]
     },
@@ -111,15 +113,15 @@ data={
         'questions': [
         {'pregunta':{
             'enunciado':'Se está recopilando y analizando datos de los usuarios/clientes agrícolas para realizar las mejoras pertinentes y llevar a cabo campañas de marketing y ventas para aumentar la adopción y expandirse al mercado.',
-            'TRL': 'TRL9'}
+            'TRL': 'TRL9_3_0'}
         },
         {'pregunta':{
             'enunciado':'Se ha desarrollado material de soporte técnico y formación para los usuarios y clientes potenciales .',
-            'TRL': 'TRL8'}
+            'TRL': 'TRL8_3_1'}
         },
         {'pregunta':{
             'enunciado':'Se ha presentado la tecnología a potenciales clientes y/o empresas agrícolas y ha recibido interés para futuras implementaciones comerciales.',
-            'TRL': 'TRL7'}
+            'TRL': 'TRL7_3_2'}
         },
     ]
     },
@@ -137,8 +139,9 @@ def root():
 @bp_agricultura_silvicultura.route('/evaluacion',methods=['POST'])
 def evaluation():
     results=[]
+    TRL=None
     count=0
-    print(request.form)
+    
     investigacion = request.form.getlist('Investigación')
     desarrollo = request.form.getlist('Desarrollo Tecnológico')
     implementacion = request.form.getlist('Implementación')
@@ -147,16 +150,35 @@ def evaluation():
     results.extend(desarrollo)
     results.extend(implementacion)
     results.extend(comercial)
-    print(results)
+    
+    options_marked=[]
+
+    for result in results:
+        try:
+            index_0=int(re.findall('[TRL0-9]+',result)[1])
+            index_1=int(re.findall('[TRL0-9]+',result)[2])
+            option=data[fields[index_0]]['questions'][index_1]['pregunta']['enunciado']
+            options_marked.append(option)
+        except:
+            continue
+
+    if len(options_marked)==0:
+        options_marked='No ha seleccionado ninguna opción'
+
     for level in condictions.keys():
-        print('nivel',level)
-        print(results.count(level))
-        print('condicion',condictions[level])
         if results.count(level) == condictions[level]:
             count+=1
         else:
             break
+
     if count == 0:
-        return 'No hay nivel de TRL'
+        TRL='No hay nivel de TRL'
     else:
-        return f'Se encuentra en TRL{count}'
+        TRL=f'Se encuentra en TRL{count}'
+    
+    window_content={
+        'answers':options_marked,
+        'TRL':TRL
+    }
+
+    return window_content
--- routes/pesca.py
+++ routes/pesca.py
@@ -1,5 +1,4 @@
 from flask import Blueprint,render_template,request
-from enum import Enum
 from random import shuffle
 import re
 
